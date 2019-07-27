38212df5fdd4a76969765da3f204515683a3c7bf
Jordan LEFEBURE <jordan.lefebure@sfr.com>
2019-07-28 01:29:26 +0200

    Add copyright

--- src/main/java/com/github/jlefebure/minio/MinioService.java
+++ src/main/java/com/github/jlefebure/minio/MinioService.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package com.github.jlefebure.minio;
 
 
@@ -66,6 +74,7 @@ public class MinioService {
 
     /**
      * Utility method which map results to items and return a list
+     * 
      * @param myObjects Iterable of results
      * @return List of items
      */
